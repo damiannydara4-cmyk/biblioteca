b6dbb2fe0b32166b989ee920445abfa1543b6dda
Dara Damianny <damiannydara4@gmail.com>
2026-08-24 21:12:16 -0300

    Initial commit

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,5 @@
+function pesquisarLivros() {
+    const termo = document.getElementById('searchInput').value;
+    console.log("Pesquisando por:", termo);
+    // Aqui entra com a chamada fetch() nas próximas fases
+}
